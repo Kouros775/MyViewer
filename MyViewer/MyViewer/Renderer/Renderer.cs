bb0f5862b049d75aacd8155b4db6f0e867781e10
kouros775 <kouros775@naver.com>
2022-03-11 15:39:41 +0900

    Add Renderer.

---
+++ MyViewer/MyViewer/Renderer/Renderer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using MyViewer.Model;
+
+
+namespace MyViewer.Renderer
+{
+    class CRenderer
+    {
+        private Dictionary<int, CModelItem> _modelItems = new Dictionary<int, CModelItem>();
+        public CRenderer()
+        {
+
+        }
+        ~CRenderer()
+        {
+
+        }
+
+        public bool AddModel(CModelItem modelItem, int index)
+        {
+            bool bRes = false;
+
+            if(IsContainIndex(index))
+            {
+                bRes = false;
+            }
+            else
+            {
+                _modelItems.Add(index, modelItem);
+                bRes = true;
+            }
+
+            return bRes;   
+        }
+
+        public bool IsContainIndex(int index)
+        {
+            bool bRes = false;
+
+            if(_modelItems.ContainsKey(index))
+            {
+                bRes = true;
+            }
+            else
+            {
+                bRes = false;
+            }
+
+
+            return bRes;
+        }
+
+        public void DrawModel()
+        {
+            foreach (var item in _modelItems)
+            {
+                item.Value.Draw();
+            }
+        }
+    }
+}
